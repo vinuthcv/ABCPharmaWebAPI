210e3034e3ad47a12b6c9da3f8d430911e5bfdb6
Vinuth <cvvinuth@gmail.com>
2020-04-05 22:45:00 +0530

    added MVC changes to invoke web APIs

--- PharmaWebAPI/Entities/Medicine.cs
+++ PharmaWebAPI/Entities/Medicine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,13 @@ namespace Entities
     public class Medicine
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Please provide name")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "Please provide Expiry date")]
         public DateTime Expiry { get; set; }
-
+        [Required(ErrorMessage = "Please provide Company name")]
         public string Company { get; set; }
-
+        [Required(ErrorMessage = "Please provide Quantity")]
         public int Quantity { get; set; }
     }
 }
