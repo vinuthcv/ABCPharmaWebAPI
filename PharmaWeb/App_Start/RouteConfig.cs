8d1e1b90637ea05b83d6d69b34083ffa06b371b7
Vinuth <cvvinuth@gmail.com>
2020-04-10 17:13:58 +0530

    deleted extra files

--- PharmaWeb/App_Start/RouteConfig.cs
+++ PharmaWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PharmaWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Pharma", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
